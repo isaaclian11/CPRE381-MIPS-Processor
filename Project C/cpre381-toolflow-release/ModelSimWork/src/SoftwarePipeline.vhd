552a01882cb3e0d6b9cfd60aaefc351877ccd9c0
aidans <aidans@iastate.edu>
2019-11-20 10:41:44 -0600

    Add pipeline register components to processor. Next: Wire IFID register

--- Project C/cpre381-toolflow-release/ModelSimWork/src/SoftwarePipeline.vhd
+++ Project C/cpre381-toolflow-release/ModelSimWork/src/SoftwarePipeline.vhd
@@ -130,6 +130,85 @@ ARCHITECTURE structure OF MIPS_Processor IS
 			o_Zero : OUT std_logic);
 
 	END COMPONENT;
+	
+	COMPONENT IFIDreg IS
+    GENERIC (N : INTEGER := 32);
+    PORT (
+      flush : IN std_logic;
+      stall : IN std_logic;
+      instr : IN std_logic_vector(N - 1 DOWNTO 0); -- instruction data
+      pcp4 : IN std_logic_vector(N - 1 DOWNTO 0); -- PC+4
+      clock : IN std_logic;
+      out_pcp4 : OUT std_logic_vector(N - 1 DOWNTO 0);
+      out_instr : OUT std_logic_vector(N - 1 DOWNTO 0));
+  END COMPONENT;
+
+  COMPONENT IDEXreg IS
+    GENERIC (N : INTEGER := 32);
+    PORT (
+      stall : IN std_logic;
+      readdata1 : IN std_logic_vector(N - 1 DOWNTO 0); -- register read data 1
+      readdata2 : IN std_logic_vector(N - 1 DOWNTO 0); -- register read data 2
+      pcp4 : IN std_logic_vector(N - 1 DOWNTO 0); -- PC+4
+      sign_ext : IN std_logic_vector(N - 1 DOWNTO 0); -- output of sign extender
+      rt : IN std_logic_vector(4 DOWNTO 0); -- instruction[20-16], Rt
+      rd : IN std_logic_vector(4 DOWNTO 0); -- instruction[15-11], Rd
+      clock : IN std_logic;
+      ctl_RegWrite : IN std_logic; -- propagate to WB
+      ctl_MemtoReg : IN std_logic; -- propagate to WB
+      ctl_MemWrite : IN std_logic; -- propagate to MEM
+      ctl_ALUOp : IN std_logic_vector(3 DOWNTO 0); -- propagate to EX
+      ctl_ALUSrc : IN std_logic; -- propagate to EX
+      ctl_RegDst : IN std_logic; -- propagate to EX
+      out_RegWrite : OUT std_logic;
+      out_MemtoReg : OUT std_logic;
+      out_MemWrite : OUT std_logic;
+      out_ALUOp : OUT std_logic_vector(3 DOWNTO 0);
+      out_ALUSrc : OUT std_logic;
+      out_RegDst : OUT std_logic;
+      out_readdata1 : OUT std_logic_vector(N - 1 DOWNTO 0);
+      out_readdata2 : OUT std_logic_vector(N - 1 DOWNTO 0);
+      out_rt : OUT std_logic_vector(4 DOWNTO 0);
+      out_rd : OUT std_logic_vector(4 DOWNTO 0);
+      out_sign_ext : OUT std_logic_vector(N - 1 DOWNTO 0);
+      out_pcp4 : OUT std_logic_vector(N - 1 DOWNTO 0));
+  END COMPONENT;
+
+  COMPONENT EXMEMreg IS
+    GENERIC (N : INTEGER := 32);
+    PORT (
+      stall : IN std_logic;
+      clock : IN std_logic;
+      ctl_RegWrite : IN std_logic; -- propagate to WB
+      ctl_MemtoReg : IN std_logic; -- propagate to WB
+      ctl_MemWrite : IN std_logic; -- propagate to MEM
+      alu_result : IN std_logic_vector(N - 1 DOWNTO 0); -- 32bit result of alu operation
+      readdata2 : IN std_logic_vector(N - 1 DOWNTO 0); -- register read data 2
+      writereg : IN std_logic_vector(4 DOWNTO 0); -- output of RegDst mux
+      out_RegWrite : OUT std_logic;
+      out_MemtoReg : OUT std_logic;
+      out_MemWrite : OUT std_logic;
+      out_aluresult : OUT std_logic_vector(N - 1 DOWNTO 0);
+      out_writedata : OUT std_logic_vector(N - 1 DOWNTO 0);
+      out_writereg : OUT std_logic_vector(4 DOWNTO 0));
+  END COMPONENT;
+
+  COMPONENT MEMWBreg IS
+    GENERIC (N : INTEGER := 32);
+    PORT (
+      stall : IN std_logic;
+      clock : IN std_logic;
+      ctl_RegWrite : IN std_logic; -- propagate to WB
+      ctl_MemtoReg : IN std_logic; -- propagate to WB
+      alu_result : IN std_logic_vector(N - 1 DOWNTO 0); -- 32bit result of alu operation
+      memreaddata : IN std_logic_vector(N - 1 DOWNTO 0); -- read data from memory module
+      writereg : IN std_logic_vector(4 DOWNTO 0); -- output of RegDst mux
+      out_RegWrite : OUT std_logic;
+      out_MemtoReg : OUT std_logic;
+      out_memreaddata : OUT std_logic_vector(N - 1 DOWNTO 0);
+      out_aluresult : OUT std_logic_vector(N - 1 DOWNTO 0);
+      out_writereg : OUT std_logic_vector(4 DOWNTO 0));
+  END COMPONENT;
 
 	-- Control flow signals 
 	SIGNAL s_ALUSrc, s_iUnsigned, s_shamt, s_memToReg, s_regDst, s_jump, s_bne, s_beq, s_jal, s_jr, s_lui : std_logic;
