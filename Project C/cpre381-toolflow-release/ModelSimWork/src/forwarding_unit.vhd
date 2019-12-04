78d04804ae606aec77e0bd2323b31a976296f89e
Sanga <isanga@iastate.edu>
2019-12-04 15:02:03 -0600

    Added forwarding and hazard detection units. Will need to add more logic

---
+++ Project C/cpre381-toolflow-release/ModelSimWork/src/forwarding_unit.vhd
@@ -0,0 +1,44 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+entity forwarding_unit is
+port(
+	instr_idex : in std_logic_vector(31 downto 0);
+	rd_exmem : in std_logic_vector(4 downto 0);
+	rd_memwb : in std_logic_vector(4 downto 0);
+	regwrite_exmem : in std_logic;
+	regwrite_memwb : in std_logic;
+	forwardA : out std_logic_vector(1 downto 0);
+	forwardB : out std_logic_vector(1 downto 0)
+);
+
+end forwarding_unit;
+
+architecture mixed of forwarding_unit is
+
+begin
+
+forwardA <= "00";
+forwardB <= "00";
+
+process(instr_idex, rd_exmem, rd_memwb, regwrite_exmem, regwrite_memwb)
+
+begin
+
+--EX hazard
+if((regwrite_exmem = '1' and rd_exmem /= "00000") and (rd_exmem = instr_idex(25 downto 21))) then
+	forwardA <= "10";
+end if;
+if((regwrite_exmem = '1' and rd_exmem /= "00000") and (rd_exmem = instr_idex(20 downto 16))) then
+	forwardB <= "10";
+end if;
+--MEM hazard
+if((regwrite_memwb = '1' and rd_memwb /= "00000") and (rd_memwb = instr_idex(25 downto 21))) then
+	forwardA <= "01";
+end if;
+if((regwrite_memwb = '1' and rd_memwb /= "00000") and (rd_memwb = instr_idex(20 downto 16))) then
+	forwardB <= "01";
+end if;
+end process;
+end mixed;
---
+++ Project C/cpre381-toolflow-release/ModelSimWork/src/hazard_detection.vhd
@@ -0,0 +1,39 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+entity hazard_detection is
+port(
+	instr_idex: in std_logic_vector(31 downto 0); --Used to check lw and rt_idex
+	instr_ifid: in std_logic_vector(31 downto 0); --Used for rs_ifid and rt_ifid
+	jump : in std_logic; --Jump signal from the control unit
+	stall : out std_logic;
+	flush_ifid : out std_logic;
+	flush_idex : out std_logic
+);
+end hazard_detection;
+
+architecture dataflow of hazard_detection is
+
+begin
+process(instr_idex, instr_ifid)
+begin
+	
+	stall <= '0';
+	flush_idex <= '0';
+	flush_ifid <= '0';
+	
+-- load hazard
+if((instr_idex(31 downto 26) = "100011" and instr_idex(20 downto 16) = instr_ifid(25 downto 21) and instr_ifid(25 downto 21) /= "00000") or (instr_idex(20 downto 16) = instr_ifid(20 downto 16) and instr_ifid(20 downto 16) /= "00000")) then
+	stall <= '1';
+	flush_idex <= '1';
+end if;
+--jump hazard
+if(jump = '1') then
+	flush_ifid <= '1';
+end if;
+
+end process;
+
+end dataflow;
+	
